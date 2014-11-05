2a9036718928ad11e73e6ad18d8c391a4e4d1baf
Antonio J. Iniesta <antonioj.iniesta@gmail.com>
2014-11-05 17:52:05 +0100

    Adding skeleton for uno

---
+++ spring-hispano/uno/uno/src/main/java/com/iniesta/springhispano/uno/App.java
@@ -0,0 +1,13 @@
+package com.iniesta.springhispano.uno;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
